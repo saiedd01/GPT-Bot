b7d7be167b9c1aa52d0b5c303173f52e252807cc
saiedd01 <sa3edahmed20@gmail.com>
2023-01-25 15:03:36 +0200

    Saied Ahmed - Create Chat Screen

---
+++ lib/Screens/chat_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'dart:io';
+
+class ChatPage extends StatefulWidget {
+  const ChatPage({super.key});
+  @override
+  State<ChatPage> createState() => _ChatPage();
+}
+
+class _ChatPage extends State<ChatPage> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
