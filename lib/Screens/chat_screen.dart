15f2bcc16225e1465a85637e69338a8bf945cc70
saiedd01 <sa3edahmed20@gmail.com>
2023-01-25 15:08:18 +0200

    Saied Ahmed- Create sendRequestFunction into chat screen

--- lib/Screens/chat_screen.dart
+++ lib/Screens/chat_screen.dart
@@ -9,7 +9,23 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPage extends State<ChatPage> {
-
+  static const apiKey = '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';
+  Future<HttpClientResponse> sendRequest(String prompt) async {
+    final client = HttpClient();
+    final request = await client
+        .postUrl(Uri.parse('https://api.openai.com/v1/completions'));
+    request.headers.set('Content-Type', 'application/json');
+    request.headers.set('Authorization', 'Bearer $apiKey');
+    request.add(utf8.encode(json.encode({
+      'model': 'text-davinci-003',
+      'prompt': prompt,
+      'max_tokens': 2048,
+      'stop': '',
+      'temperature': 0.7,
+    })));
+    final response = await request.close();
+    return response;
+  }
 
   @override
   Widget build(BuildContext context) {
